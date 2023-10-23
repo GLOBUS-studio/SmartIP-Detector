e17ac8dec5cd69003fdf77bcda8fd300cae5535f
GLOBUS.studio <admin@globus.studio>
2023-10-23 17:48:13 +0300

    Update

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # SmartIP-Detector
 
-A universal class for determining the IP address of the client.
+`SmartIP-Detector` is a robust PHP class designed to accurately determine the client's IP address, regardless of the environment. This solution is critical for applications requiring precise user location, ranging from security features like fraud detection to functionality enhancements such as content localization.
 
-* Support for checking the header CloudFlare
-* Address validation (IPv4 / IPv6)
+## Features
 
-## Use:
+- **Cloudflare Support:** Seamlessly retrieves the user's original IP address forwarded by Cloudflare using the `CF-Connecting-IP` header. Essential for applications behind Cloudflare's protective infrastructure.
+- **Comprehensive IP Validation:** Rigorously validates IP addresses to ensure they conform to IPv4 or IPv6 standards, thereby avoiding erroneous data and potential security vulnerabilities.
+- **Dependability:** Gracefully handles a plethora of scenarios by employing a series of fallback mechanisms, ensuring an accurate IP detection regardless of server configuration or HTTP headers presence.
+
+## Installation
+
+Simply incorporate the `Smart_IP` class into your PHP project. You can manually add the `Smart_IP.php` file to your project or potentially use a package manager if the class is available as a package in the future (e.g., via Composer).
+
+```php
+require_once 'path_to/Smart_IP.php';
 ```
-$test = new Smart_IP();
-echo $test->ip();
 
-// 101.120.84.52
+## Usage
+
+Instantiate a new Smart_IP object and then call the ip() method to retrieve the client's IP address. The method returns a string containing a valid IP address if available; otherwise, it returns a default value.
+
+```php
+$detector = new Smart_IP();
+echo $detector->ip();
+
+// Output: "101.120.84.52" (example output, actual result will vary)
 ```
+
+## Contribution
+
+Contributions to improve SmartIP-Detector are welcome! Feel free to fork the repository and submit pull requests. For major changes, please open an issue first to discuss what you would like to change. Please ensure to update tests as appropriate.
+
+## License
+
+SmartIP-Detector is open-sourced software licensed under the MIT license.
--- dist/smart_ip.php
+++ dist/smart_ip.php
@@ -1,56 +1,83 @@
 <?php
 
+/**
+ * Class Smart_IP
+ *
+ * This class is designed to retrieve and validate the real IP address of a user.
+ */
 class Smart_IP {
 
-    public $null = '-'; //null safety
-    public $user_ip = '';
-    public $cf = false;
-    public $valid = false;
+    /**
+     * @var string $null Default value for an invalid IP address.
+     */
+    private $null = '-';
 
-/**
- * Get real user IP address
- *
- * @return string IP address.
- */
+    /**
+     * @var string|null $user_ip The user's IP address.
+     */
+    private $user_ip = null;
+
+    /**
+     * @var bool $cf Flag indicating whether the IP was obtained from Cloudflare.
+     */
+    private $cf = false;
+
+    /**
+     * @var bool $valid Flag indicating whether the IP address is valid.
+     */
+    private $valid = false;
+
+    /**
+     * Retrieves the real IP address of a user.
+     *
+     * @return string|null The IP address.
+     */
     public function ip() {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR']) {
-            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ".") > 0 && strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ",") > 0) {
-                $ip = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
-                $this->user_ip = trim($ip[0]);
-            } elseif (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ".") > 0 && strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ",") === false) {
-                $this->user_ip = trim($_SERVER['HTTP_X_FORWARDED_FOR']);
-            }
+        // Check for the presence of the 'X-Forwarded-For' header.
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // Splitting the string that contains possible IP addresses.
+            $ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $this->user_ip = trim($ips[0]); // Taking the first from the list.
+        } elseif (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            // Get IP from Cloudflare.
+            $this->user_ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $this->cf = true;
+        } else {
+            // Otherwise, just take the remote address.
+            $this->user_ip = $_SERVER["REMOTE_ADDR"];
         }
-        if (!isset($this->user_ip)) {
-            if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-                $this->user_ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
-                $this->cf = true;
-            } else {
-                $this->user_ip = $_SERVER["REMOTE_ADDR"];
-            }
-        }
-       if($this->validate() == true){
-           return $this->user_ip;
-       }
+
+        return $this->validate() ? $this->user_ip : $this->null;
     }
-    
-/**
- * Validate IP address
- *
- * @return bool If IPv4 or IPv6 correct return TRUE
- */    
-    public function validate() {
-        if (!filter_var($this->user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) && !filter_var($this->user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+
+    /**
+     * Validates the IP address (IPv4 or IPv6).
+     *
+     * @return bool Returns TRUE if the IP address is valid.
+     */
+    private function validate() {
+        if (!filter_var($this->user_ip, FILTER_VALIDATE_IP)) {
+            // If the IP fails validation, set it to null and return false.
             $this->user_ip = $this->null;
             $this->valid = false;
-            return $this->valid;
         } else {
+            // If the IP is valid, set to true.
             $this->valid = true;
-            return $this->valid;
         }
+
+        return $this->valid;
     }
 
+    /**
+     * Returns information about whether the IP was obtained through Cloudflare.
+     *
+     * @return bool
+     */
+    public function isCloudflare() {
+        return $this->cf;
+    }
 }
 
+// Example of using the class.
 $test = new Smart_IP();
-$test->ip();
+echo $test->ip();
